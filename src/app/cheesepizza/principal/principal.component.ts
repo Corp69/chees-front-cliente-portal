0121f0681d54dcf5a08804653e11c980badff458
Corpsey69 <elizandro.carballo.casillas@eccs.com.mx>
2026-01-28 20:32:35 -0600

    cheespizza - msj de carga

--- src/app/cheesepizza/principal/principal.component.ts
+++ src/app/cheesepizza/principal/principal.component.ts
@@ -1,20 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import ImportsModule from '@shared/primeng/ImportsModule';
 import { Router } from '@angular/router';
+import MsjCarga from '@shared/cheesepizza/mensajes/msj-carga/msj-carga';
 
 @Component({
   standalone: true,
   imports: [
     //prime NG
     ImportsModule,
+    //shared msj - carga
+    MsjCarga
   ],
-  providers: [MessageService],
+  providers: [
+    MessageService
+  ],
   templateUrl: './principal.component.html',
   styles: ``,
 })
 export default class PrincipalComponent {
   private readonly url = inject(Router);
+
+  //--=============================================================================================================
+  //?-- cheespizza - Variables de control
+  public Blocked = signal<boolean>(true);
+
+
 
   //navegar
   public irCapturardatos() {
---
+++ src/app/shared/cheesepizza/mensajes/msj-carga/msj-carga.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cheese-msj-carga',
+  imports: [],
+  templateUrl: './msj-carga.html',
+  styleUrl: './msj-carga.css'
+})
+export default class MsjCarga {
+
+}
